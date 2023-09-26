e0c6a7dc779a58f1a292409dc42d2bcc28dcff27
Thaliehln <71643277+Thaliehln@users.noreply.github.com>
2023-09-26 14:03:03 +0200

    Add fuzzy search based on labels

--- Form.py
+++ Form.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import Levenshtein
+import skrub
 
 def get_normalized_edit_distance(s1, s2):
     try:
@@ -39,20 +40,20 @@ class Form:
                  in_xlsx,
                  survey):
         
-        survey_df   = pd.read_excel(in_xlsx, sheet_name="survey")
-        choices_df  = pd.read_excel(in_xlsx, sheet_name="choices")
-        settings_df = pd.read_excel(in_xlsx, sheet_name="settings")
+        # Raw data from XLSForm
+        self._survey_df   = pd.read_excel(in_xlsx, sheet_name="survey").reset_index()
+        self._choices_df  = pd.read_excel(in_xlsx, sheet_name="choices")
+        self._settings_df = pd.read_excel(in_xlsx, sheet_name="settings")
         
         # General form attributes
-        self._id               = settings_df.loc[0]["form_id"]
-        self._title            = settings_df.loc[0]["form_title"]
-        self._version          = settings_df.loc[0]["version"],
-        self._default_language = settings_df.loc[0]["default_language"],
+        self._id               = self._settings_df.at[0, "form_id"]
+        self._title            = self._settings_df.at[0, "form_title"]
+        self._version          = self._settings_df.at[0, "version"]
+        self._default_language = self._settings_df.at[0, "default_language"]
         self._survey           = survey
 
         # Load questions
-        survey_df = survey_df.reset_index()
-        questions = survey_df[survey_df["name"].notnull()]
+        questions = self._survey_df[self._survey_df["name"].notnull()]
         questions = questions[questions["type"] != "note"]
         questions = questions[["index",
                                "type",
@@ -73,43 +74,72 @@ class Form:
         self._questions = questions.reset_index(drop=True)
 
     # Instance Methods
+
+    def getSurvey(self):
+
+        return self._survey_df
+    
+    def getChoices(self):
+
+        return self._choices_df
+    
+    def getSettings(self):
+
+        return self._settings_df
+
     """This method returns the unique identifier of the form."""
+
     def getID(self):
+
         return self._id
 
     """This method returns the title of the form."""
+
     def getTitle(self):
+
         return self._title
     
     """This method returns the version information of the form."""
+
     def getVersion(self):
+
         return self._version
     
     """This method returns the default language information of the form."""
+
     def getDefaultLanguage(self):
+
         return self._default_language
     
     """This method returns the survey type associated with the form."""
+
     def getSurvey(self):
+
         return self._survey
     
     def getQuestions(self):
+
         return self._questions
     
     """This method is intended to return the parent of the form. However, the parent attribute (_parent) is not set within the class, so this method may not provide the expected functionality without additional implementation."""
+    
     def getParent(self):
+
         return self._parent
     
     """This method takes another Form object (form) as an argument and compares various attributes of the current form with the attributes of the provided form.
     It returns a formatted string containing comparison results."""
-    def compare(self, form):
-        out1 = self.compareID(form)
-        out2 = self.compareVersion(form)
-        out = "{}\n{}".format(out1, out2)
+    def compare(self, f):
+
+        out1 = self.compareID(f)
+        out2 = self.compareVersion(f)
+        out3 = self.compareDefaultLanguage(f)
+        out = "{}\n{}\n{}".format(out1, out2, out3)
         return out
     
     """This method compares the version attribute of the current form with the version attribute of the provided form. It returns a string indicating whether the versions are identical or different."""
     def compareVersion(self, f):
+
         cver = f.getVersion()
         out = ""
         if self._version != cver:
@@ -120,6 +150,7 @@ class Form:
     
     """This method compares the unique identifier attribute of the current form with the identifier attribute of the provided form. It returns a string indicating whether the form IDs are identical or different."""
     def compareID(self, f):
+
         cid = f.getID()
         out = ""
         if self._id != cid:
@@ -128,7 +159,18 @@ class Form:
             out = "Form ID is identical: {}".format(self._id)
         return out
     
+    def compareDefaultLanguage(self, f):
+
+        cdl = f.getDefaultLanguage()
+        out = ""
+        if self._default_language != cdl:
+            out = "Default languages are different: {} and {}".format(self._default_language, cdl)
+        else:
+            out = "Default language is identical: {}".format(self._default_language)
+        return out
+    
     def addedQuestions(self, f):
+
         out = pd.merge(left = self._questions,
                        right = f.getQuestions(),
                        on = "name",
@@ -138,34 +180,81 @@ class Form:
         out = out[["index_y",
                    "name",
                    "type_y",
-                   "label::English (en)_y",
-                   "group_id_y",
-                   "group_lbl_y"]].rename(columns = {"index_y": "row",
-                                                     "type_y": "type",
-                                                     "label::English (en)_y": "label",
-                                                     "group_id_y": "group_id",
-                                                     "group_lbl_y": "group_lbl"})
+                   "label::English (en)_y"]] \
+                    .rename(columns = {"index_y": "row",
+                                       "type_y": "type",
+                                       "label::English (en)_y": "label"}) \
+                    .astype({'row':'int'})
+
+        if (out.shape[0] == 0):
+            out = None
+
+        if out is not None:
+            tmp1 = self._questions.copy(deep=True)
+            tmp1 = tmp1[tmp1["label::English (en)"].notnull()]
+            out = skrub.fuzzy_join(out[["row", "name", "label"]],
+                                   tmp1[["index", "name", "label::English (en)"]],
+                                   left_on='label',
+                                   right_on='label::English (en)',
+                                   how='left',
+                                   match_score=0,
+                                   return_score=True)
+            out = out[["name_x",
+                    "label",
+                    "name_y",
+                    "label::English (en)",
+                    "matching_score"]] \
+                    .rename(columns = {"name_x": "name",
+                                        "name_y": "name_of_closest_lbl",
+                                        "label::English (en)": "closest_lbl"}) \
+                    .fillna("")
+
         return out
     
     def modifiedLabel(self, f):
+
         out = pd.merge(left = self._questions,
                        right = f.getQuestions(),
                        on = "name",
                        how = 'inner')
         out["edit_distance"] = out.apply(lambda row: get_normalized_edit_distance(s1 = row["label::English (en)_x"], s2 = row["label::English (en)_y"]), axis = 1)
-        out = out[(out["label::English (en)_x"].notnull()) & (out["edit_distance"] > 0.2)]
-        out = out.reset_index(drop = True)
-        out = out[["name",
-                   "index_x",
-                   "index_y",
-                   "label::English (en)_x",
-                   "label::English (en)_y"]].rename(columns = {"index_x": "row1",
-                                                               "index_y": "row2",
-                                                               "label::English (en)_x": "label1",
-                                                               "label::English (en)_y": "label2"})
-        return out
+
+        # Major modifications
+        major = out[(out["label::English (en)_x"].notnull()) & (out["edit_distance"] > 0.2)]
+        major = major.reset_index(drop = True)
+        major = major[["name",
+                       "index_x",
+                       "label::English (en)_x",
+                       "index_y",
+                       "label::English (en)_y"]] \
+                        .rename(columns = {"index_x": "row1",
+                                           "index_y": "row2",
+                                           "label::English (en)_x": "label1",
+                                           "label::English (en)_y": "label2"}) \
+                        .astype({'row1':'int', 'row2':'int'})
+        if (major.shape[0] == 0):
+            major = None
+        
+        # Minor modifications
+        minor = out[(out["label::English (en)_x"].notnull()) & (out["edit_distance"] > 0) & (out["edit_distance"] <= 0.2)]
+        minor = minor.reset_index(drop = True)
+        minor = minor[["name",
+                       "index_x",
+                       "label::English (en)_x",
+                       "index_y",
+                       "label::English (en)_y"]] \
+                        .rename(columns = {"index_x": "row1",
+                                           "index_y": "row2",
+                                           "label::English (en)_x": "label1",
+                                           "label::English (en)_y": "label2"}) \
+                        .astype({'row1':'int', 'row2':'int'})
+        if (minor.shape[0] == 0):
+            minor = None
+
+        return major, minor
     
     def removedQuestions(self, f):
+
         out = pd.merge(left = self._questions,
                        right = f.getQuestions(),
                        on = "name",
@@ -173,12 +262,19 @@ class Form:
         out = out[out["type_x"].notnull() & out["type_y"].isnull()]
         out = out.reset_index(drop = True)
         out = out[["index_x",
-                   "type_x",
                    "name",
+                   "type_x",
                    "label::English (en)_x",
                    "group_id_x",
-                   "group_lbl_x"]].rename(columns = {"index_x": "row",
-                                                     "type_x": "type"})
+                   "group_lbl_x"]] \
+                   .rename(columns = {"index_x": "row",
+                                       "type_x": "type",
+                                       "label::English (en)_x": "label",
+                                       "group_id_x": "group_id",
+                                       "group_lbl_x": "group_lbl"}) \
+                   .astype({'row':'int'})
+        if (out.shape[0] == 0):
+            out = None
         return out
     
     """Please note that the compare, compareVersion, and compareID methods are designed to provide comparison functionality but should be used with care, as they rely on the assumption that certain attributes of the form are set correctly during initialization."""
